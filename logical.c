586c00b3c125d1d9df3b75c0718cd43415e1d029
ShreeVidya-HS <shreevidyasvhs@gmail.com>
2024-11-04 15:11:28 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=20,b=12;
+    if((a>5)&&(b>5)){
+        printf("a & b are greater than 5");
+    }
+    else{
+        printf("a & b are not greater than 5");
+    }
+}
