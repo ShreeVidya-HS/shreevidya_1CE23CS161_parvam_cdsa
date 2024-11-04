e3abe12dfebb06425d9319c2773e521ead0a07fd
ShreeVidya-HS <shreevidyasvhs@gmail.com>
2024-11-04 12:59:09 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=20;
+    int c=20;
+    int eq,neq,gr,less;
+    if(a==b){
+        printf("a is equal to b");
+    }
+    else
+        printf("a is not equal to b");
+    if(a>b){
+        printf("a is greter than b");
+    }
+    else
+        printf("\nb is greater than a");
+    
+    
+}
