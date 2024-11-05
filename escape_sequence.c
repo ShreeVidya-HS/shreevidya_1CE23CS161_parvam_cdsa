e2700635761c3398f441f3f23ddb84994dae6812
ShreeVidya-HS <shreevidyasvhs@gmail.com>
2024-11-05 10:12:23 +0530

    Add files via upload

---
+++ escape_sequence.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("\"hello world\"\n"); //\t(tab), \n(new line), \b(backspace)
+    printf("monica\'s dress is pretty\n\n");
+    printf("NAME\tUSN\t\tDate\n");
+    printf("Vidya\t123\t\t3 Nov");
+}
